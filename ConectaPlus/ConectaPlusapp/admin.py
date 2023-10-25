7a766d3f245a931639cc99ea7ab11111efb3ecc9
Diogo Henrique de Melo Correia <diogohenriquedemelo28@gmail.com>
2023-10-25 12:14:27 -0300

    Complemento da introdução, settings, admin, models

--- ConectaPlus/ConectaPlusapp/admin.py
+++ ConectaPlus/ConectaPlusapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Perfil, Projeto
+
+class ListandoPerfil(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links =("id", "name")
+    search_fields = ("name",)
+
+admin.site.register(Perfil, ListandoPerfil)
+
+
+class ListandoProjeto(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links =("id", "name")
+    search_fields = ("name",)
+
+admin.site.register(Projeto, ListandoProjeto)
--- ConectaPlus/ConectaPlusapp/models.py
+++ ConectaPlus/ConectaPlusapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Perfil(models.Model):
+    name    = models.CharField(max_length=50)
+    idade   = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
+
+
+class Projeto(models.Model):
+    name        = models.CharField(max_length= 100)
+    descricao   = models.TextField(max_length= 500, blank = True, null= True)
+    
+    def __str__(self):
+        return self.name
+    
+    
